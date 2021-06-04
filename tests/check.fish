b2abdd033cb3e100c466914321336de448f104c3
Shoma Kokuryo <s.kokuryo@gmail.com>
2021-06-05 00:39:20 +0900

    fix

---
+++ tests/check.fish
@@ -0,0 +1,8 @@
+source ${HOME}/.asdf/asdf.fish
+asdf --version
+asdf list golang
+asdf shell golang 1.15.13
+go version
+asdf list terraform
+asdf shell terraform 0.15.5
+terraform version
---
+++ tests/check.sh
@@ -0,0 +1,8 @@
+. ${HOME}/.asdf/asdf.sh
+asdf --version
+asdf list golang
+asdf shell golang 1.15.13
+go version
+asdf list terraform
+asdf shell terraform 0.15.5
+terraform version
